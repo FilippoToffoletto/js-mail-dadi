fb275cb896411b4ddacce7ea0f157107b6507ec5
FilippoToffoletto <filippotoffoletto@hotmail.it>
2022-10-14 15:37:51 +0200

    [js]verifica mail

---
+++ js/script.js
@@ -0,0 +1,50 @@
+/*
+Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo.
+
+**Gioco dei dadi**
+
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.
+Prima di partire a scrivere codice poniamoci qualche domanda:
+Che ci sia un array da qualche parte?
+Se dobbiamo confrontare qualcosa che “cosa” ci serve?
+*/
+const mailRegistrate = [
+    'ciao@gmail.com', 
+    'mario@gmail.com',
+    'mariorossi@gmail.com', 
+    'filippo@gmail.com',
+    'hello@gmail.com', 
+    'html@gmail.com',
+    'luca@gmail.com', 
+    'fabio@gmail.com',
+    'hi@gmail.com', 
+    'css@gmail.com',
+    'fabrizio@gmail.com', 
+    'noccioline@gmail.com',
+    'francesco@gmail.com', 
+    'finestra@gmail.com',
+    'gmail@gmail.com', 
+    'giulia@gmail.com',
+]
+const verification = document.getElementById('mail');
+
+const mail = prompt('Scrivi la tua mail per verificare la tua registrazione');
+
+
+for(let i = 0; i < mailRegistrate.length; i++){
+    let result = (mailRegistrate.includes(mail));
+    if(result === true){
+        verification.innerHTML = `La tua mail è presente nel nostro gestionale.`
+    }else{
+        verification.innerHTML = `La tua mail non è presente nel nostro gestionale.`
+    }
+    console.log(result);
+}
+
+
+
+
+
