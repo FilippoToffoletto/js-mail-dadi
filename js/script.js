c9bc7d45032f57844940024149ebf0268e3ba9e8
FilippoToffoletto <filippotoffoletto@hotmail.it>
2022-10-14 16:12:27 +0200

    [js]lancio dadi

--- js/script.js
+++ js/script.js
@@ -1,16 +1,4 @@
-/*
-Chiedi all’utente la sua email,
-controlla che sia nella lista di chi può accedere,
-stampa un messaggio appropriato sull’esito del controllo.
 
-**Gioco dei dadi**
-
-Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
-Stabilire il vincitore, in base a chi fa il punteggio più alto.
-Prima di partire a scrivere codice poniamoci qualche domanda:
-Che ci sia un array da qualche parte?
-Se dobbiamo confrontare qualcosa che “cosa” ci serve?
-*/
 const mailRegistrate = [
     'ciao@gmail.com', 
     'mario@gmail.com',
@@ -37,14 +25,42 @@ const mail = prompt('Scrivi la tua mail per verificare la tua registrazione');
 for(let i = 0; i < mailRegistrate.length; i++){
     let result = (mailRegistrate.includes(mail));
     if(result === true){
-        verification.innerHTML = `La tua mail è presente nel nostro gestionale.`
+        verification.innerHTML = `La tua mail è presente nel nostro gestionale.`;
     }else{
-        verification.innerHTML = `La tua mail non è presente nel nostro gestionale.`
+        verification.innerHTML = `La tua mail non è presente nel nostro gestionale.`;
     }
     console.log(result);
 }
 
+/*
+
+**Gioco dei dadi**
+
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.
+Prima di partire a scrivere codice poniamoci qualche domanda:
+Che ci sia un array da qualche parte?
+Se dobbiamo confrontare qualcosa che “cosa” ci serve?
+
+*/
 
 
+
+
+const button = document.querySelector('#bottone');
+const result = document.getElementById('result');
+
+button.addEventListener('click', function(){
+    let randomNumberUser = Math.floor(Math.random() * 12 + 2);
+    let randomNumberPc = Math.floor(Math.random() * 12 + 2);
+    if(randomNumberPc < randomNumberUser){
+        result.innerHTML = 'hai vinto';
+    }else{
+        result.innerHTML = 'hai perso';
+    }
+    // let randomNumberPc = document.getElementById('numberpc');
+    // let randomNumberUser = document.getElementById('numberuser');
+
+})
 
 
